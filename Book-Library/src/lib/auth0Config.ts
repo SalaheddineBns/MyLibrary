58e6d61bf5112534a74cef99b1647ddda030a7cc
Salaheddine Benkhanous <benkhanoussalah@gmail.com>
2026-05-01 19:10:57 +0200

    feat: use env var for Auth0 audience to support production deployment

--- Book-Library/src/lib/auth0Config.ts
+++ Book-Library/src/lib/auth0Config.ts
@@ -3,7 +3,7 @@
 export const auth0Config = {
  clientId: 'Iw3lRLeJLaulMVcfvHW5ePkUEnKqd3kS',
  issuer: "dev-6qtr30vk60x15j0l.us.auth0.com",
- audience: "http://localhost:8080",
+ audience: process.env.REACT_APP_AUTH0_AUDIENCE || "http://localhost:8080",
  redirectUri: window.location.origin+"/login/callback",
  scope: 'openid profile email'
 }
